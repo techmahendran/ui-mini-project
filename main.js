fb5ad4b4ea047269eeec0eb9289e7fa3dc8c27f9
techmahendran <mahendran361999@gmail.com>
2021-04-04 16:41:05 +0530

    add a file

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('output').style.visibility = 'hidden';
+
+document.getElementById('box1').addEventListener('input', (e) =>{
+    // console.log(123);
+
+document.getElementById('output').style.visibility = 'visible';
+
+let pound = e.target.value;
+
+document.getElementById('gramsOutput').innerHTML = pound/0.0022046;
+
+document.getElementById('kgOutput').innerHTML = pound/2.2046;
+
+document.getElementById('ozOutput').innerHTML = pound*16;
+})
